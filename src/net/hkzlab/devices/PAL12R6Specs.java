c54f52968c898f0f776808368882556c72a1c366
Baglio Tabifata <tabaglio@posteo.net>
2020-07-24 21:17:16 +0200

    Add empty pal specs types

---
+++ src/net/hkzlab/devices/PAL12R6Specs.java
@@ -0,0 +1,29 @@
+package net.hkzlab.devices;
+
+public class PAL12R6Specs implements PALSpecsInterface {
+
+    @Override
+    public int getINPinsTotal() {
+        // TODO Auto-generated method stub
+        return 0;
+    }
+
+    @Override
+    public int getIOPinsTotal() {
+        // TODO Auto-generated method stub
+        return 0;
+    }
+
+    @Override
+    public int getOPinsTotal() {
+        // TODO Auto-generated method stub
+        return 0;
+    }
+
+    @Override
+    public int getROPinsTotal() {
+        // TODO Auto-generated method stub
+        return 0;
+    }
+    
+}
---
+++ src/net/hkzlab/devices/PALSpecsInterface.java
@@ -0,0 +1,8 @@
+package net.hkzlab.devices;
+
+public interface PALSpecsInterface {
+    public int getINPinsTotal();
+    public int getIOPinsTotal();
+    public int getOPinsTotal();
+    public int getROPinsTotal();
+}
