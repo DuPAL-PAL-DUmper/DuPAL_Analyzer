3de82cd552758576ffff21e8ce29e9f20bfb43e9
Fabio Battaglia <hkzlabnet@gmail.com>
2020-09-07 19:50:49 +0200

    Begin defining the states

---
+++ src/main/java/info/hkzlab/dupal/analyzer/palanalisys/OutState.java
@@ -0,0 +1,11 @@
+package info.hkzlab.dupal.analyzer.palanalisys;
+
+public class OutState {
+    int hiz_state = 0;
+    int o_state = 0;
+    int io_state = 0;
+
+    public OutState() {
+
+    }
+}
