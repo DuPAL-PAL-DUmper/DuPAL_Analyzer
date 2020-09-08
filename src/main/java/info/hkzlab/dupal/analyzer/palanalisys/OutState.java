3a267df9755b7034721e359b2cc2e97704dfe55d
Fabio Battaglia <hkzlabnet@gmail.com>
2020-09-08 08:18:55 +0200

    Return status clone from OutState

--- src/main/java/info/hkzlab/dupal/analyzer/palanalisys/OutState.java
+++ src/main/java/info/hkzlab/dupal/analyzer/palanalisys/OutState.java
@@ -5,12 +5,16 @@ public class OutState {
     public final static int IDX_IO = 1;
     public final static int IDX_HIZ = 2;
 
-    public final int[] status;
+    private final int[] status;
     private final OutLink[] links;
 
     public OutState(int o_state, int io_state, int hiz_state, int totLinks) {
         status = new int[]{o_state, io_state, hiz_state};
         links = new OutLink[totLinks];
+    }
+
+    public int[] getStatus() {
+        return status.clone();
     }
 
     public OutLink getOutLinkAtIdx(int idx) {
