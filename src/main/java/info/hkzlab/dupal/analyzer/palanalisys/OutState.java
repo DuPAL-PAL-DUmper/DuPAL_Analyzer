721c92508dc835049f4ebea3753f78a28dbf4b4e
Fabio Battaglia <hkzlabnet@gmail.com>
2020-09-08 07:50:53 +0200

    Add other utility functions for an OutState

--- src/main/java/info/hkzlab/dupal/analyzer/palanalisys/OutState.java
+++ src/main/java/info/hkzlab/dupal/analyzer/palanalisys/OutState.java
@@ -1,18 +1,27 @@
 package info.hkzlab.dupal.analyzer.palanalisys;
 
-import java.util.ArrayList;
-
 public class OutState {
     public final static int IDX_O = 0;
     public final static int IDX_IO = 1;
     public final static int IDX_HIZ = 2;
 
     public final int[] status;
-    private OutLink[] links;
+    private final OutLink[] links;
 
     public OutState(int o_state, int io_state, int hiz_state, int totLinks) {
         status = new int[]{o_state, io_state, hiz_state};
         links = new OutLink[totLinks];
+    }
+
+    public OutLink getOutLinkAtIdx(int idx) {
+        return links[idx];
+    }
+
+    public boolean setOutLinkAtIdx(OutLink link, int idx) {
+        if(links[idx] != null) return false;
+        links[idx] = link;
+
+        return true;
     }
 
     @Override
